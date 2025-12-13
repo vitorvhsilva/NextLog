889a3aa9501b0e85305ca4d6e68bf9480c98a47e
vitorvhsilva <dndragonbr@gmail.com>
2025-12-13 17:12:06 -0300

    uploading order service

---
+++ order-service/src/main/kotlin/br/com/nextlog/order_service/OrderServiceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.nextlog.order_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderServiceApplication>(*args)
+}
